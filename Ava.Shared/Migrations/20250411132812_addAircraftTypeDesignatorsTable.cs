f7732e1d1ebc2c8d57c0a0a4e007c2289941e67c
Danijel-James Wynyard <danijel@repasscloud.com>
2025-04-11 23:28:42 +1000

    update migrations for new table

---
+++ Ava.Shared/Migrations/20250411132812_addAircraftTypeDesignatorsTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ava.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class addAircraftTypeDesignatorsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AircraftTypeDesignators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ICAOCode = table.Column<string>(type: "text", nullable: true),
+                    IATATypeCode = table.Column<string>(type: "text", nullable: true),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    WikipediaLink = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AircraftTypeDesignators", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AircraftTypeDesignators");
+        }
+    }
+}
