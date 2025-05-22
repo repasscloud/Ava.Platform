dc0e888fa16ef8baa421d32931ebfaa390b43745
Danijel-James Wynyard <danijel@repasscloud.com>
2025-05-22 22:07:35 +1000

    add attribs, used by AvaTerminal3

---
+++ Ava.Shared/Models/Kernel/Client/Attribs/SupportedCurrency.cs
@@ -0,0 +1,16 @@
+namespace Ava.Shared.Models.Kernel.Client.Attribs;
+
+public class SupportedCurrency
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public required string Iso4217 { get; set; }
+
+    [Required]
+    public required string Symbol { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+}
