0ad303ce4ea4fc5199690c27d97efa3e4422304f
Danijel-James Wynyard <danijel@repasscloud.com>
2025-05-25 18:26:25 +1000

    v2.3.5

--- update-ava.zsh
+++ update-ava.zsh
@@ -5,8 +5,22 @@
 
 # Variables
 VERSION_FILE="ava_version"
-AVAAPI_SUBMODULE_DIR="ava.api.docker"
-AVAAPI_COMPOSE_FILE="${AVAAPI_SUBMODULE_DIR}/compose.yaml"
+
+AVAAPI_SUBMODULE_DIR="ava.api"
+AVAAPI_DCKR_FILE="${AVAAPI_SUBMODULE_DIR}/Dockerfile.API"
+AVAAPI_DCKR_LOCAL_FILE="Dockerfile.API"
+
+AVAAPI_DCKR_SUBMODULE_DIR="ava.api.docker"
+AVAAPI_DCKR_COMPOSE_FILE="${AVAAPI_DCKR_SUBMODULE_DIR}/compose.yaml"
+
+AVATERM3_SUBMODULE_DIR="ava.terminal3"
+AVATERM3_VERSION_FILE="${AVATERM3_SUBMODULE_DIR}/Models/Static/AppVersion.cs"
+
+AVASHARED_SUBMODULE_DIR="ava.shared"
+AVASHARED_VERSION_FILE="${AVASHARED_SUBMODULE_DIR}/Models/Static/VersionInfo.cs"
+
+AVADEPLOY_DCKR_SUBMODULE_DIR="ava.deploy.docker"
+
 
 # Ensure file exists
 if [[ ! -f $VERSION_FILE ]]; then
@@ -44,30 +58,124 @@ echo $new_version >| $VERSION_FILE
 # Feedback
 echo "Updated ava_version → $new_version"
 
-# ava.api.docker
-if [[ -f $AVAAPI_COMPOSE_FILE ]]; then
-  echo "🔄 Updating image tag in $AVAAPI_COMPOSE_FILE"
-  sed -i -E \
-    "s#^([[:space:]]*image:[[:space:]]*repasscloud/ava-api:).*#\1${new_version}#" \
-    "$AVAAPI_COMPOSE_FILE"
-  echo "✅ Updated $AVAAPI_COMPOSE_FILE → image: repasscloud/ava-api:${new_version}"
 
-  # ommit & push in submodule
-  echo "🔄 Committing changes in $AVAAPI_SUBMODULE_DIR"
-  pushd "$AVAAPI_SUBMODULE_DIR" > /dev/null
+# ava.shared
+if [[ -f $AVASHARED_VERSION_FILE ]]; then
+  echo "🔄 Updating version in $AVASHARED_VERSION_FILE"
+  sed -i -E \
+    's#^([[:space:]]*public static string ClientVersion[[:space:]]*\{[^\}]*\}[[:space:]]*=[[:space:]]*)"[^"]*";$#\1"'"${new_version}"'";#' \
+    "$AVASHARED_VERSION_FILE"
+  echo "✅ Updated $AVASHARED_VERSION_FILE → ${new_version}"
+
+  # commit & push in submodule
+  echo "🔄 Committing changes in $AVASHARED_SUBMODULE_DIR"
+  pushd "$AVASHARED_SUBMODULE_DIR" > /dev/null
     git add .
     git commit -m "v${new_version}"
     git push
+    # git push origin main:dev --force
+    git tag -s v${new_version}" -m v${new_version}"
+    git push --tags
   popd > /dev/null
   echo "✅ Pushed submodule update"
 else
-  echo "⚠️  $AVAAPI_COMPOSE_FILE not found; skipping image update and commit"
+  echo "⚠️  $AVASHARED_VERSION_FILE not found; skipping image update and commit"
 fi
 
-# Dockerfile.API
-sed -i -E \
+
+# ava.api
+if [[ -f $AVAAPI_DCKR_FILE ]]; then
+  echo "🔄 Updating version in $AVAAPI_DCKR_FILE"
+  sed -i -E \
   "s#^LABEL[[:space:]]*version=\"[^\"]*\"#LABEL version=\"${new_version}\"#" \
-  Dockerfile.API
+    $AVAAPI_DCKR_FILE
+  echo "✅ Updated $AVAAPI_DCKR_FILE → ${new_version}"
+
+  # commit & push in submodule
+  echo "🔄 Committing changes in $AVAAPI_SUBMODULE_DIR"
+  pushd "$AVASHARED_SUBMODULE_DIR" > /dev/null
+    git add .
+    git commit -m "v${new_version}"
+    git push
+    git push origin main:dev --force
+    git tag -s v${new_version}" -m v${new_version}"
+    git push --tags
+  popd > /dev/null
+  echo "✅ Pushed submodule update"
+else
+  echo "⚠️  $AVAAPI_DCKR_FILE not found; skipping image update and commit"
+fi
+
+if [[ -f $AVAAPI_DCKR_LOCAL_FILE ]]; then
+  echo "🔄 Updating version in $AVAAPI_DCKR_LOCAL_FILE"
+  sed -i -E \
+  "s#^LABEL[[:space:]]*version=\"[^\"]*\"#LABEL version=\"${new_version}\"#" \
+    $AVAAPI_DCKR_LOCAL_FILE
+  echo "✅ Updated $AVAAPI_DCKR_LOCAL_FILE → ${new_version}"
+else
+  echo "⚠️  $AVAAPI_DCKR_LOCAL_FILE not found; skipping image update and commit"
+fi
+
+
+# ava.api.docker
+if [[ -f $AVAAPI_DCKR_COMPOSE_FILE ]]; then
+  echo "🔄 Updating image tag in $AVAAPI_DCKR_COMPOSE_FILE"
+  sed -i -E \
+    "s#^([[:space:]]*image:[[:space:]]*repasscloud/ava-api:).*#\1${new_version}#" \
+    "$AVAAPI_DCKR_COMPOSE_FILE"
+  echo "✅ Updated $AVAAPI_DCKR_COMPOSE_FILE → image: repasscloud/ava-api:${new_version}"
+
+  # commit & push in submodule
+  echo "🔄 Committing changes in $AVAAPI_DCKR_SUBMODULE_DIR"
+  pushd "$AVAAPI_DCKR_SUBMODULE_DIR" > /dev/null
+    git add .
+    git commit -m "v${new_version}"
+    git push
+    git tag -s v${new_version}" -m v${new_version}"
+    git push --tags
+  popd > /dev/null
+  echo "✅ Pushed submodule update"
+else
+  echo "⚠️  $AVAAPI_DCKR_COMPOSE_FILE not found; skipping image update and commit"
+fi
+
+
+# ava.deploy.docker
+cp "${VERSION_FILE}" "${AVADEPLOY_DCKR_SUBMODULE_DIR}/${VERSION_FILE}"
+echo "🔄 Committing changes in $AAVADEPLOY_DCKR_SUBMODULE_DIR"
+  pushd "$AVADEPLOY_DCKR_SUBMODULE_DIR" > /dev/null
+    git add .
+    git commit -m "v${new_version}"
+    git push
+    git tag -s v${new_version}" -m v${new_version}"
+    git push --tags
+  popd > /dev/null
+echo "✅ Pushed submodule update"
+
+
+# ava.terminal3
+if [[ -f $AVATERM3_VERSION_FILE ]]; then
+  echo "🔄 Updating version in $AVATERM3_VERSION_FILE"
+  sed -i -E \
+    's#^([[:space:]]*public static readonly string VersionInfo *= *)"[^"]*";$#\1"'"${new_version}"'";#' \
+    "$AVATERM3_VERSION_FILE"
+  echo "✅ Updated $AVATERM3_VERSION_FILE → ${new_version}"
+
+  # commit & push in submodule
+  echo "🔄 Committing changes in $AVATERM3_SUBMODULE_DIR"
+  pushd "$AVATERM3_SUBMODULE_DIR" > /dev/null
+    git add .
+    git commit -m "v${new_version}"
+    git push
+    git push origin main:dev --force
+    git tag -s v${new_version}" -m v${new_version}"
+    git push --tags
+  popd > /dev/null
+  echo "✅ Pushed submodule update"
+else
+  echo "⚠️  $AVAAPI_DCKR_COMPOSE_FILE not found; skipping image update and commit"
+fi
+
 
 # ava.platform
 git add .
@@ -78,3 +186,4 @@ git push origin main:dev --force
 # docker containers build
 git tag --annotate --sign "v${new_version}" -m "v${new_version}"
 git push origin "v${new_version}"
+
